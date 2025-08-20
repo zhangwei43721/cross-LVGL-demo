2a6f82df8fd466a405de4ea949d3bb8cb3d9a1d6
Skyforever <zhangwei4372@outlook.com>
2025-08-20 19:52:39 +0800

    init

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main.c.o"
+  "CMakeFiles/main.dir/main.c.o.d"
+  "CMakeFiles/main.dir/mouse_cursor_icon.c.o"
+  "CMakeFiles/main.dir/mouse_cursor_icon.c.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
